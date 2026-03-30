a3a8db417c4acc1e91afc0af9bf7a3b1b2433563
joaomidowz <jgcustodio2005@gmail.com>
2026-03-30 19:54:01 -0300

    feat: atualizando para chamar o app com uma class

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main () {
-  runApp(
-    MaterialApp(
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatlessWidget {
+  MeuApp((super.key));
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
